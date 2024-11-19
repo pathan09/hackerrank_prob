97b7fcc216b1416c896bdefdace1dab2853525a6
AyakuthPathan <pathan.gems@mopa.gov.bd>
2024-11-19 12:19:54 +0600

    init

---
+++ src/main/java/com/apathan/Main.java
@@ -0,0 +1,10 @@
+package com.apathan;
+
+/**
+ * Created by AyakuthPathan on 19-Nov-24.
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
